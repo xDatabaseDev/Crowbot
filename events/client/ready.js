189cfde6728a81629382a4d0928893dfc0ae9988
Neyzoxx <144907719+Neyz0xx@users.noreply.github.com>
2024-08-03 11:17:04 -0700

    Add files via upload

---
+++ events/client/ready.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const disbut = require("discord-buttons")
+const db = require("quick.db")
+
+require('http').createServer((req, res) => res.end(`
+ dams je suis un putain de génie
+`)).listen(3000)
+
+module.exports = (client) => {
+    console.log(`- Connecté à ${client.user.username}`)
+    client.guilds.cache.map(async guild => {
+        await guild.members.fetch().catch(e => { })
+    })
+}
