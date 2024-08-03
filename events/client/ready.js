1249d721087519204c4b5718d2ae6eee65911a47
Neyzoxx <144907719+Neyz0xx@users.noreply.github.com>
2024-08-03 12:16:32 -0700

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,9 +2,7 @@ const Discord = require("discord.js");
 const disbut = require("discord-buttons")
 const db = require("quick.db")
 
-require('http').createServer((req, res) => res.end(`
- dams je suis un putain de génie
-`)).listen(3000)
+
 
 module.exports = (client) => {
     console.log(`- Connecté à ${client.user.username}`)
